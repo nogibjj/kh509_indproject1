0c0d8d638c4dac7c7af17ee0207739a76884ab4f
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-09-18 00:10:22 +0000

    all code complete

--- lib.py
+++ lib.py
@@ -6,7 +6,21 @@ def calc_desc_stat(dataset_col):
     out=dataset_col.describe()
     return round(out,2)
 
-def boxplot_of_col(df_wanted, col):
-    df_wanted.boxplot(column=col)
+def boxplot_of_cols(df_wanted, col1=None, col2=None, col3=None, file_name=None):
+    list_of_columns = []
+    if col1:
+        list_of_columns.append(col1)
+    if col2:
+        list_of_columns.append(col2)
+    if col3:
+        list_of_columns.append(col3)
+    if len(list_of_columns) == 0:
+        return print("There are no columns to plot")
+    
+    df_to_plot = df_wanted[list_of_columns]
+    ax = df_to_plot.boxplot()
+    
+    if file_name:
+        plt.savefig(file_name)
+    plt.savefig("boxplot")
     plt.show()
-    plt.savefig(str(col.columns) + ".png")
--- stat1.py
+++ stat1.py
@@ -1,9 +1,9 @@
 import pandas as pd 
 import matplotlib.pyplot as plt
-from lib import calc_desc_stat, boxplot_of_col 
+from lib import calc_desc_stat, boxplot_of_cols
 
-df1 = pd.read_csv('iris.csv')
+df1 = pd.read_csv('datasets/iris.csv')
 
 calc_desc_stat(df1['petal.length'])
 
-boxplot_of_col(df1,'petal.length')
+boxplot_of_cols(df1,'petal.length', 'sepal.length', file_name='test')
